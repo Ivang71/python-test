17c3812ec399fc79767f0d9b2698e145395e45c5
Ivang71 <artemtolv@gmail.com>
2024-04-05 09:38:07 +0500

    make parser more robust

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,6 +1,7 @@
 from utils import curl
 from bs4 import BeautifulSoup
-import re, urllib.parse
+from urllib.parse import quote_plus
+import re, time, random
 
 class Parser:
     db = None
@@ -21,12 +22,19 @@ class Parser:
     
     
     def getCompanyEmails(self, company):
-        # for keyword in ['hr', 'contact', 'info', 'support']:
         domain = re.search(r"(?:https?://)?(?:www\.)?([^\.]+\.[^\.]+)", company['website']).group(1)
-        query = f"contact @{domain}"
-        searchResult = curl(f'https://www.google.com/search?q={urllib.parse.quote_plus(query)}')
-        return list(set(re.findall(r'\b[a-z.]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', searchResult)))
-        #     time.sleep(random.randint(1, 2))
+        emails = []
+        # keywords = ['hr', 'email', 'contact', 'info', 'support']
+        keywords = ['hr', 'email', 'contact']
+        i = 0
+        while(len(emails) < 4 and i < len(keywords)):
+            query = f"{keywords[i]} @{domain}"
+            searchResult = curl(f'https://www.google.com/search?q={quote_plus(query)}')
+            emails.extend(list(set(re.findall(r'\b[a-z.]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', searchResult))))
+            i += 1
+            time.sleep(random.randint(1, 2))
+        
+        return emails if emails else None
     
     
     
