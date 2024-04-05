28f208f05346794df8affb079ee0fb7e54b3f5ca
Ivang71 <artemtolv@gmail.com>
2024-04-05 09:38:25 +0500

    create  main logic

--- llm/__init__.py
+++ llm/__init__.py
@@ -9,7 +9,7 @@ class Llm:
         self.api_key = api_key
     
     
-    def __prompt(self, prompt):
+    def __prompt(self, prompt) -> str | None:
         url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + self.api_key
         headers = {"Content-Type": "application/json"}
         data = {"contents": [{"parts": [{"text": prompt}]}]}
@@ -21,8 +21,10 @@ class Llm:
     
     def pickEmails(self, emails: list[str]):
         response = self.__prompt(f"Pick the best email or emails to send my resume to.\
-            If there's no good email, then bad ones are good enough, like contact or support email.\
-            You're response should be json-parseable and consist of array of emails like ['a@b.c', 'hr@example.com'].\
+            If there's no good email, then bad ones, like contact or support or any random email.\
+            You cannot not pick an email. Your response contains at least one email.\
+            Your response should be json-parseable and consist of array of emails like ['a@b.c', 'hr@example.com'].\
+            Your response should contain only double quotes and be json parseable.\
             If they differ only by subdomain, pick the higher level one.\
             There might not be an appropriate email, in such case pick less appropriate ones on the principle of less evil.\
             Comments or any other symbols are allowed. You are not allowed to output anything except for the array.\
@@ -32,15 +34,25 @@ class Llm:
     
     
     def writeCoverLetter(self, company_name, resume_text, site_text, email):
-        return self.__prompt(f'Write a proper concise cover letter for the company {company_name} I\'m cold emailing out of blue.\
-            Using my resume text: "{resume_text}".\
-            And using text from the company website: "{site_text}".\
-            Focus on how valuable I would be for the company.\
-            Do not retell my resume, instead write a good cover letter.\
-            Do not over exaggerate my accomplishments.\
-            You must paste the link to my portfolio website "https://ivang71.github.io/portfolio".\
-            End with clear CTA to contact me.\
-            I\'m sending to the email {email}.\
-            You\'re response must contain only the cover letter text with no additional comments.')
+        response = '['
+        while ('[' in response):
+            response = self.__prompt(f'Write a proper concise cover letter for the company {company_name} I\'m cold emailing out of blue.\
+                The length of the letter can be a few paragraphs at max.\
+                Using my resume text: "{resume_text}".\
+                And using text from the company website: "{site_text}".\
+                Focus on how valuable I would be for the company.\
+                Must be very aligned with companies values.\
+                This is not a template, it\'s an actual email, so it cannot contain template stuff like [fill in something].\
+                Must not contain field that need filling, this is the final text.\
+                Do not retell my resume, instead write a good cover letter.\
+                Do not over exaggerate my accomplishments.\
+                You must paste the link to my portfolio website "https://ivang71.github.io/portfolio".\
+                You can mention that I have attached my resume to the email.\
+                The response should be properly formatted and each paragram should be surrounded by new lines.\
+                Focus on benefit for them, not babble about my experience.\
+                End with clear CTA to contact me.\
+                I\'m sending to the email {email}, if it\'s not an appropriate email, insert apology at the beginning with request to forward to hr.\
+                You\'re response must contain only the cover letter text with no additional comments.')
+        return response
 
     
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from data.db import JobSearchDatabase
 from parsers import niche, Parser
 from llm import Llm
 from search import find_hr_emails
-from utils import curl
+from utils import curl, log_email
 import re, urllib.parse, time, random, os
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
@@ -14,41 +14,50 @@ llm_api_key = os.environ.get('GEMINI_API_KEY')
 
 
 def main():
-    pass
-    # db = JobSearchDatabase()
-    # parser = Parser(db)
-    # llm = Llm(llm_api_key)
+    db = JobSearchDatabase()
+    parser = Parser(db)
+    llm = Llm(llm_api_key)
     gmail = Gmail()
-    gmail.send('rewwer356@gmail.com', 'Test 1', 'No credentials found. Please set up Application Default Credentials.')
-    # # niche.parseEverything(db)
+    # niche.parseEverything(db)
     
-    # companies = db.get_companies()
-    # companies = [c for c in companies if c['message_state'] == 'none' and c['website'] != None]
-    
-    # company = companies[41]
-    
-    # site_text = parser.getCompanyText(company) # getting text and emails
-    # if site_text == None:
-    #     db.update_message_state(company['id'], 'client_side_generation')
-    #     # continue # website unavailable, skip this company
-    # emails = parser.getCompanyEmails(company)
-    # # attempts = 5
-    # # while(len(emails) < 1):
-    # #     emails = parser.getCompanyEmails(company)
-    # #     attempts -= 1
+    companies = db.get_companies()
+    companies = [c for c in companies if
+                 c['website'] != None
+                 and c['message_state'] == 'none']
+
+    for company in companies:
+        print(f"wokring on {company['name']}")
         
-    # if len(emails) > 1:
-    #     emails = llm.pickEmails(emails)
-    #     # db.update_company(company['id'], emails=emails)
-    
-    # resume_text = PdfReader('resume.pdf').pages[0].extract_text().replace('\n', ' ')
-    
-    # coverLetter = llm.writeCoverLetter(company['name'], resume_text, site_text, random.choice(emails))
-    # # send resume to the email
-    
-    
-    # # for company in companies:
-    # #     print(company)
+        # Website text
+        site_text = parser.getCompanyText(company)
+        if site_text == None:
+            db.update_message_state(company['id'], 'client_side_generation')
+            continue # website unavailable, skip this company
+        
+        # Emails
+        emails = parser.getCompanyEmails(company)
+        if not emails:
+            print(f"skipping {company['name']}")
+            db.update_message_state(company['id'], 'did_not_find_email')
+            continue # couldn't get any emails, skip
+        attempts = 5
+        while(len(emails) < 1 and attempts > 0):
+            print(len(emails))
+            emails = parser.getCompanyEmails(company)
+            attempts -= 1
+        if len(emails) > 1:
+            emails = llm.pickEmails(emails)
+            db.update_company(company['id'], emails=emails)
+        
+        # Cover letter
+        resume_text = PdfReader('resume.pdf').pages[0].extract_text().replace('\n', ' ')
+        to_email = random.choice(emails)
+        cover_letter = llm.writeCoverLetter(company['name'], resume_text, site_text, to_email)
+        
+        # Mailing
+        subject = f"Frontend Developer seeking opportunity at {company['name']}"
+        gmail.send(to_email, subject, cover_letter)
+        log_email(to_email, subject, cover_letter)
 
 
 if __name__ == "__main__":
