3cb962a8627de4a23f1d056922c818b48e86f8fb
Ivang71 <artemtolv@gmail.com>
2024-04-05 09:37:42 +0500

    add email logging

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,12 @@
-import subprocess, random
+import subprocess, random, datetime
 
 
 def curl(url):
     command = 'curl_chrome' + random.choice(['99', '100', '101', '104', '107', '110', '116'])
     return subprocess.run([command, url, '-sL'], stdout=subprocess.PIPE).stdout.decode('utf-8')
 
+
+def log_email(to_email, subject, cover_letter):
+    with open('sent_emails.txt', 'a') as f:
+        f.write(f"{datetime.datetime.now()}\n{to_email}\n{subject}\n\n{cover_letter}\n{'—'*200}\n\n")
+    
